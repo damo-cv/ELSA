439f3c20468572ad79e05ba72e06b001adbd19ef
F.S.Fire <1252693592@qq.com>
2023-03-07 14:39:05 +0800

    Update elsa.py

--- det/mmdet/models/backbones/elsa/elsa.py
+++ det/mmdet/models/backbones/elsa/elsa.py
@@ -138,7 +138,7 @@ def elsa_op(features, ghost_mul, ghost_add, h_attn, lam, gamma,
     else:
         B, C, H, W = features.shape
         _pad = kernel_size // 2 * dilation
-        features = Function.unfold(
+        features = F.unfold(
             features, kernel_size=kernel_size, dilation=dilation, padding=_pad, stride=stride) \
             .reshape(B, C, kernel_size ** 2, H * W)
         ghost_mul = ghost_mul.reshape(B, C, kernel_size ** 2, 1)
